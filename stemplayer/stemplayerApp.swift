13526fee667fd36f18ba6d06b949045f9990bfa1
arrrshia <andrewalmasi@gmail.com>
2025-09-20 11:01:41 -0500

    Initial Commit

---
+++ stemplayer/stemplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stemplayerApp.swift
+//  stemplayer
+//
+//  Created by Andrew Arshia Almasi on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct stemplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
